ebd6c1ab2056057f2e652b3d4522b40df4c5ed14
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-12-01 11:08:52 +1000

    basic framework for all containers included

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,7 @@
 git clone https://github.com/ThugPigeon653/ecosystem-model.git
+copy ecosystem-model/src/main.py frontend/main.py
 copy ecosystem-model/src/ecosystem.py ecosystem/ecosystem.py
+copy ecosystem-model/src/world_visualizer.py map/map.py
+python frontend/transform_main.py
+python ecosystem/transform_ecosystem.py
+python map/transform_map.py
